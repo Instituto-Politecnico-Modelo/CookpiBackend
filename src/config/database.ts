0354b3b9a5b884c66b474f99b403f36f07679c0d
EnsaladaDeFrutas <nico.caces07@gmail.com>
2025-06-23 17:42:02 -0300

    dockerfile

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,18 +1,18 @@
 import { Sequelize } from 'sequelize';
-
+/*
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'database.sqlite', // o ruta relativa/absoluta al archivo
   logging: false, // opcional: desactiva los logs de SQL
 });
 
-/*
+*/
 const sequelize = new Sequelize({
   dialect: "mysql",
   database: "cookpi",
   username: "alumno",
   password: "alumnoipm",
-  host: "localhost",
+  host: "172.16.4.249",
 });
-*/
+
 export default sequelize;
